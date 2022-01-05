78fcb2df11186996c42129708a2bcf5a9b32c450
augustine saidimu <47994458+augustinesaidimu@users.noreply.github.com>
2022-01-05 17:28:56 +0300

    fix: modified install script

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env zsh
-
 DOTFILES=~/.dotfiles
 
+# check that the repo exists
+if [ ! -e $DOTFILES ]; then
+    pushd $HOME
+    git clone git@github.com:augustinesaidimu/.dotfiles.git
+    popd
+fi
+
+# install dotfiles
 pushd $DOTFILES
 for folder in `ls $DOTFILES`; do
     if [ -d "$folder" ]; then

05f03bd06686567ae8d350d0b9c7d6c92cb22c2b
augustine saidimu <47994458+augustinesaidimu@users.noreply.github.com>
2022-01-23 16:16:13 +0300

    updated cmux configs

--- cmus/.config/cmus/lib.pl
+++
@@ -1,2 +0,0 @@
-/home/augustine/music/Music/Code.Session/y2mate.com - Chillstep Music for Programming  Cyber  Coding.mp3
-/home/augustine/music/Music/Code.Session/y2mate.com - Programming  Designing  Hacking  Coding  Focus  Music .mp3

2b7d0c0b519d3fbb477022db07367302db770e63
Saidimu <47994458+asaidimu@users.noreply.github.com>
2024-01-25 14:17:34 +0300

    build: update configs

--- zsh/.config/zsh/zshrc.zsh
+++ zsh/.config/zsh/zshrc.zsh
@@ -65,3 +65,9 @@ autoload -Uz $fpath[1]/*(.:t)
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/augustine/google-cloud-sdk/path.zsh.inc' ]; then . '/home/augustine/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/augustine/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/augustine/google-cloud-sdk/completion.zsh.inc'; fi
